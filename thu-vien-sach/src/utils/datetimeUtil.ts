63b661edbbf4d35520021cd43a1e32abc13f4857
kuro2904 <trung2001sgp@gmail.com>
2024-12-02 16:52:24 +0700

    refactor DateUtil function

--- thu-vien-sach/src/utils/datetimeUtil.ts
+++ thu-vien-sach/src/utils/datetimeUtil.ts
@@ -1,5 +1,6 @@
 import dayjs from "dayjs";
 
-export const reFormatToDDMMYY = (date: string): string => {
+export const reFormatToDDMMYY = (date?: string): string => {
+  if(!date) return "";
   return dayjs(date).format("DD/MM/YYYY");
 };
