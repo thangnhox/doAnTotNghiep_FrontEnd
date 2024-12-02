bdda8ea31dfdc22f4a9a78066705e9722badc510
kuro2904 <trung2001sgp@gmail.com>
2024-12-02 16:54:13 +0700

    Add Firebase config

---
+++ thu-vien-sach/src/firebase/firebaseConfig.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, getRedirectResult, GoogleAuthProvider, signInWithRedirect, signOut } from "firebase/auth";
+import { RemoveAuth } from "../redux/authSlice";
+import { useDispatch } from "react-redux";
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const authProvider = new GoogleAuthProvider();
+export const firebaseAuth = getAuth(app);
