532d4bdecb7c4c7f2b769d95b5f294f8e32271a3
kuro2904 <trung2001sgp@gmail.com>
2024-12-02 16:56:44 +0700

    Update Router

--- thu-vien-sach/src/routes/MainRouter.tsx
+++ thu-vien-sach/src/routes/MainRouter.tsx
@@ -1,31 +1,36 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import BookDetailPage from '../pages/books/BookDetailPage'
-import { Layout, theme } from 'antd'
-import { Content, Footer } from 'antd/es/layout/layout'
-import UserDetailPage from '../pages/UserDetailPage'
+import { Layout } from 'antd'
+import { Content } from 'antd/es/layout/layout'
 import HomePage from '../pages/HomePage'
 import NotFoundPage from '../pages/NotFoundPage'
 import HeaderComponent from '../components/HeaderComponent'
 import AuthPage from '../pages/AuthPage'
 import VerifyPage from '../pages/VerifyPage'
 import FooterComponent from '../components/FooterComponent'
-import Sider from 'antd/es/layout/Sider'
 import BookPage from '../pages/books/BookPage'
 import AuthorsPage from '../pages/authors/AuthorsPage'
+import BookReader from '../pages/books/BookReader'
+import UserInformationPage from '../pages/users/UserInformationPage'
+import { useSelector } from 'react-redux'
+import { authState } from '../redux/authSlice'
+import CategoriesPage from '../pages/CategoriesPage'
 
 const MainRouter = () => {
+  const autState = useSelector(authState)
   return (
     <BrowserRouter>
       <Layout className='p-0 m-0'>
         <HeaderComponent />
-          <Content className='mx-5 App-content m-3'>
+          <Content className='mx-5 m-3'>
           <Routes>
             <Route index element={<HomePage />} />
-            <Route path='/user/:userId' element={<UserDetailPage />} />
+            <Route path='/user/user-detail' element={<UserInformationPage user={ autState.user }  />} />
             <Route path='/book/:bookId' element={<BookDetailPage />} />
             <Route path='/books' element={<BookPage/>} />
-            <Route path='/book/:bookId/read' element={<BookDetailPage />} />
+            <Route path='/book/:bookId/read' element={<BookReader />} />
+            <Route path='/categories' element={<CategoriesPage />} />
             <Route path='/authors' element={<AuthorsPage/>} />
             <Route path='/login' element={<AuthPage action='LOGIN' />} />
             <Route path='/register' element={<AuthPage action='REGISTER' />} />
