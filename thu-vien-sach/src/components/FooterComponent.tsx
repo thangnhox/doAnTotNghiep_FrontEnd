e1471df7428497e09cd14bdae5a78263ef359385
kuro2904 <trung2001sgp@gmail.com>
2024-12-05 15:34:02 +0700

    Update Footer

--- thu-vien-sach/src/components/FooterComponent.tsx
+++ thu-vien-sach/src/components/FooterComponent.tsx
@@ -1,14 +1,17 @@
-import { Footer } from 'antd/es/layout/layout'
-import React from 'react'
+import { Footer } from "antd/es/layout/layout";
+import React from "react";
 
 const FooterComponent = () => {
   return (
-    <Footer className='bg-white mt-5' >
+    <Footer className="bg-white mt-5">
       <div className="d-flex flex-column gap-3 pt-5 ">
-        <span>Created by <br/>  <strong>Lương Trung</strong> <br/> and <br/> <strong>Tôn Đức Thắng</strong></span>
+        <span>
+          Created by <br /> <strong>Lương Trung</strong> <br /> and <br />{" "}
+          <strong>Tôn Đức Thắng</strong>
+        </span>
       </div>
     </Footer>
-  )
-}
+  );
+};
 
-export default FooterComponent
+export default FooterComponent;
