feaad2c4a55a485308be04e0e9a0ae321f597495
kuro2904 <trung2001sgp@gmail.com>
2024-12-11 17:56:24 +0700

    update api handle for blob file

--- thu-vien-sach/src/remotes/apiHandle.ts
+++ thu-vien-sach/src/remotes/apiHandle.ts
@@ -3,10 +3,12 @@ import axiosInstace from "./axiosConfig";
 export const handleAPI = async (
   url: string,
   data?: any,
-  method?: "get" | "put" | "post" | "delete"
+  method?: "get" | "put" | "post" | "delete",
+  responseType?: "json" | "blob"
 ) => {
   return await axiosInstace(url, {
     method: method ?? "get",
     data,
+    responseType: responseType ?? "json",
   });
 };
