d763645676ef7e707915f223011c0b673a98952a
kuro2904 <trung2001sgp@gmail.com>
2024-12-11 17:53:53 +0700

    add Edit UserInformation

--- thu-vien-sach/src/components/user/UserInfo.tsx
+++ thu-vien-sach/src/components/user/UserInfo.tsx
@@ -1,17 +1,65 @@
 import { UserOutlined } from "@ant-design/icons";
-import { Avatar, Card, Typography } from "antd";
-import React from "react";
+import {
+  Avatar,
+  Button,
+  Card,
+  Divider,
+  Modal,
+  Typography,
+  Form,
+  Input,
+  DatePicker,
+  message,
+} from "antd";
+import { useForm } from "antd/es/form/Form";
+import dayjs from "dayjs";
+import { useState } from "react";
+import { AxiosResponse } from "axios";
+import { handleAPI } from "../../remotes/apiHandle";
 
 interface Props {
   fullName: string;
   email: string;
   avatarUrl?: string;
-  birthYead?: string;
+  birthYear: string;
 }
 
 const UserInfo = (props: Props) => {
-  const { fullName, email, avatarUrl, birthYead } = props;
+  const { fullName, email, avatarUrl, birthYear } = props;
   const { Text, Title } = Typography;
+  const [formLoading, setFormLoading] = useState<boolean>(false);
+  const [isOpenChangeUserInfoModal, setOpenChangeUserInfoModal] =
+    useState<boolean>(false);
+  const [form] = useForm();
+
+  const onCancelModal = () => {
+    form.resetFields();
+    setOpenChangeUserInfoModal(false);
+  };
+
+  const onPerformEdit = async () => {
+    try {
+      setFormLoading(true);
+      const name: string = form.getFieldValue("name");
+      const birthYear: number = dayjs(form.getFieldValue("birthYear")).year();
+      const reqInfo = {
+        name,
+        birthYear,
+      };
+      const res: AxiosResponse = await handleAPI(`user/update`, reqInfo, "put");
+      if (res.status === 200) {
+        message.success(`Cập nhật thông tin thành công`);
+        form.resetFields();
+        setOpenChangeUserInfoModal(false);
+        window.location.reload();
+      }
+    } catch (error: any) {
+      console.log(error);
+      message.error(`Đã xảy ra lỗi! Vui lòng thử lại sau`);
+    } finally {
+      setFormLoading(false);
+    }
+  };
 
   return (
     <Card>
@@ -29,10 +77,36 @@ const UserInfo = (props: Props) => {
             <strong>Email</strong>: {email}
           </Text>
           <Text>
-            <strong>Năm sinh</strong>: {birthYead ?? "N/a"}
+            <strong>Năm sinh</strong>: {birthYear}
           </Text>
+          <Divider type="vertical" />
+          <Button onClick={() => setOpenChangeUserInfoModal(true)}>
+            Cập nhật
+          </Button>
         </div>
       </div>
+      <>
+        <Modal
+          title="Cập nhật thông tin người dùng"
+          open={isOpenChangeUserInfoModal}
+          closable={false}
+          onCancel={onCancelModal}
+          onOk={() => form.submit()}
+          loading={formLoading}
+        >
+          <Form form={form} onFinish={onPerformEdit}>
+            <Form.Item name="email" label="Email" initialValue={email}>
+              <Input disabled />
+            </Form.Item>
+            <Form.Item name="name" label="Tên" initialValue={fullName}>
+              <Input />
+            </Form.Item>
+            <Form.Item name="birthYear" label="Năm sinh" initialValue={dayjs()}>
+              <DatePicker />
+            </Form.Item>
+          </Form>
+        </Modal>
+      </>
     </Card>
   );
 };
