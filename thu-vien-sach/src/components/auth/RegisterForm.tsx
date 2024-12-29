4453907a1d9b78332d998801d47ad9cbe16a38e0
kuro2904 <trung2001sgp@gmail.com>
2024-12-29 14:23:04 +0700

    Add check blank

--- thu-vien-sach/src/components/auth/RegisterForm.tsx
+++ thu-vien-sach/src/components/auth/RegisterForm.tsx
@@ -15,39 +15,45 @@ const RegisterForm = () => {
   const register = async () => {
     const password: string = registerForm.getFieldValue('password');
     const confirm: string = registerForm.getFieldValue('confirmPassword');
-    if(password !== confirm){
-      registerForm.setFields([
-        {
-          name:'password',
-          errors:['Mật khẩu và xác nhận phải giống nhau']
-        },
-        {
-          name:'confirmPassword',
-          errors:['Mật khẩu và xác nhận phải giống nhau']
-        }
-      ]);
-    }
-    const name: string = registerForm.getFieldValue('name');
-    const email: string = registerForm.getFieldValue('email');
 
-    try {
-      setLoading(true)
-      const req = {
-        email,
-        password,
-        birthYear,
-        name
+    if (password.trim() === '' || confirm.trim() === '') {
+      message.error('Mật khẩu không được trống')
+    } else {
+      if (password !== confirm) {
+        registerForm.setFields([
+          {
+            name: 'password',
+            errors: ['Mật khẩu và xác nhận phải giống nhau']
+          },
+          {
+            name: 'confirmPassword',
+            errors: ['Mật khẩu và xác nhận phải giống nhau']
+          }
+        ]);
       }
-      const res: AxiosResponse = await handleAPI(`user/createUser`,req, 'post');
-      if(res.status === 200) {
-        setOpenModal(true) 
+      const name: string = registerForm.getFieldValue('name');
+      const email: string = registerForm.getFieldValue('email');
+
+      try {
+        setLoading(true)
+        const req = {
+          email,
+          password,
+          birthYear,
+          name
+        }
+        const res: AxiosResponse = await handleAPI(`user/createUser`, req, 'post');
+        if (res.status === 200) {
+          setOpenModal(true)
+        }
+      } catch (error: any) {
+        console.log(error)
+        message.error(error.response?.data)
+      } finally {
+        setLoading(false)
       }
-    } catch (error:any) {
-      console.log(error)
-      message.error(error.response?.data)
-    }finally{
-      setLoading(false)
     }
+
 
   }
 
@@ -58,84 +64,84 @@ const RegisterForm = () => {
 
   return (
     <>
-    <Card
-      loading={isLoading}
-      title='Đăng ký thành viên'>
-      <Form
-        form={registerForm}
-        onFinish={register}>
-        <Form.Item
-          name={'name'}
-          label={'Tên'}
-          rules={[
-            {
-              required: true,
-              min: 2,
-              message: 'Tên không hợp lệ'
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={'email'}
-          label={'Email'}
-          rules={[
-            {
-              required: true,
-              min: 3,
-              message: "Email không được trống"
-            }
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          name={'password'}
-          label={'password'}
-          rules={[
-            {
-              required: true,
-              min: 2,
-              message: 'Mật khẩu không hợp lệ'
-            }
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          name={'confirmPassword'}
-          label={'Xác nhận mật khẩu'}
-          rules={[
-            {
-              required: true,
-              min: 2,
-              message: 'Mật khẩu không hợp lệ'
-            }
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          name={'birtYear'}
-          label={'Năm sinh'}
-          rules={[
-            {
-              required: true,
-              message: 'Năm hợp lệ'
-            }
-          ]}
-        >
-          <DatePicker onChange={onChange} picker="year" />
-        </Form.Item>
-        <Button type='primary' onClick={() => registerForm.submit()} loading={isLoading}>Đăng ký</Button>
-      </Form>
-    </Card>
-    <Modal title ="Thông báo" open={isOpenModal} centered closable={false} footer={
-      <Button type='primary' onClick={()=> navigate('/',{replace:true})} >Ok</Button>
-    } >
-      <p><span>Đăng ký thành công, hãy kiểm tra email của bạn để xác thực tài khoản!!</span></p>
-    </Modal>
+      <Card
+        loading={isLoading}
+        title='Đăng ký thành viên'>
+        <Form
+          form={registerForm}
+          onFinish={register}>
+          <Form.Item
+            name={'name'}
+            label={'Tên'}
+            rules={[
+              {
+                required: true,
+                min: 2,
+                message: 'Tên không hợp lệ'
+              }
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={'email'}
+            label={'Email'}
+            rules={[
+              {
+                required: true,
+                min: 3,
+                message: "Email không được trống"
+              }
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            name={'password'}
+            label={'password'}
+            rules={[
+              {
+                required: true,
+                min: 2,
+                message: 'Mật khẩu không hợp lệ'
+              }
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name={'confirmPassword'}
+            label={'Xác nhận mật khẩu'}
+            rules={[
+              {
+                required: true,
+                min: 2,
+                message: 'Mật khẩu không hợp lệ'
+              }
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name={'birtYear'}
+            label={'Năm sinh'}
+            rules={[
+              {
+                required: true,
+                message: 'Năm hợp lệ'
+              }
+            ]}
+          >
+            <DatePicker onChange={onChange} picker="year" />
+          </Form.Item>
+          <Button type='primary' onClick={() => registerForm.submit()} loading={isLoading}>Đăng ký</Button>
+        </Form>
+      </Card>
+      <Modal title="Thông báo" open={isOpenModal} centered closable={false} footer={
+        <Button type='primary' onClick={() => navigate('/', { replace: true })} >Ok</Button>
+      } >
+        <p><span>Đăng ký thành công, hãy kiểm tra email của bạn để xác thực tài khoản!!</span></p>
+      </Modal>
     </>
   )
 }
